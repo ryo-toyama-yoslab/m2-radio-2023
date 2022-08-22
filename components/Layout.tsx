bc7c8dcae7237401741939dfdfa452c3dc42f85e
katasu-git <katasu9923@gmail.com>
2022-08-22 23:36:40 +0900

    add scroll

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,10 @@ import { tw } from "@twind";
 
 export default function header(props: { children: JSX.Element[] }) {
   return (
-    <div class={tw`p-6 mx-auto max-w-screen-md`}>
+    <div
+      class={tw`p-6 mx-auto max-w-screen-md overflow-scroll`}
+      style={{ "overflow-scrolling": "touch" }}
+    >
       {props.children}
     </div>
   );
