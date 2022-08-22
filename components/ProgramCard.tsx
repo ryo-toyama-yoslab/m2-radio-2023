213cf6386cdeab746c6045d83f36772bd9c25611
katasu-git <katasu9923@gmail.com>
2022-08-22 18:55:57 +0900

    add card

---
+++ components/ProgramCard.tsx
@@ -0,0 +1,35 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+
+type Props = {
+  iconName: string;
+  title: string;
+  personalityLastNames: string[];
+  overview: string;
+  playtime: number;
+};
+
+export default function header(props: Props) {
+  return (
+    <div class={tw`mt-4 p-6 rounded-[10px] border-4 border-[#9FA6ED]`}>
+      <div class={tw`flex flex-row justify-start`}>
+        <img
+          src={`/${props.iconName}_3d.png`}
+          alt={props.iconName}
+          class={tw`h-12`}
+        />
+        <div class={tw`flex flex-col ml-6`}>
+          <div class={tw`text-xl font-bold`}>{props.title}</div>
+          <div class={tw`text-xs text-[#9FA6ED] mt-1`}>
+            {props.personalityLastNames.join(" / ")}
+          </div>
+        </div>
+      </div>
+      <div class={tw`text-sm mt-6`}>{props.overview}</div>
+      <div class={tw`flex justify-end font-bold text-sm mt-6`}>
+        {props.playtime}分
+      </div>
+    </div>
+  );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -4,6 +4,7 @@ import { tw } from "@twind";
 import Header from "../components/Header.tsx";
 import Layout from "../components/Layout.tsx";
 import H1 from "../components/H1.tsx";
+import ProgramCard from "../components/ProgramCard.tsx";
 
 export default function Home() {
   return (
@@ -11,6 +12,13 @@ export default function Home() {
       <Header />
       <Layout>
         <H1 text="Now Playing" />
+        <ProgramCard
+          iconName="t-rex"
+          title="Mr.林のマシンガントーク ~ジョジョ編~"
+          personalityLastNames={["林", "酒井"]}
+          overview="林は、ジョジョが持つ「マシンガン」を使って、自分の身体を覚醒させる。"
+          playtime={60}
+        />
         <div class={tw`mt-8`} />
         <H1 text="Personalities" />
         <div class={tw`mt-8`} />
