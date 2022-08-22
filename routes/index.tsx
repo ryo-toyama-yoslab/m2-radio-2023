d31a26bf2b1863abf89327876087ba4a508f681b
katasu-git <katasu9923@gmail.com>
2022-08-22 18:12:46 +0900

    add header

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,12 +1,23 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      hello
+    <div class={tw`text-[#45454D]`}>
+      <header
+        class={tw`p-6 h-16 bg-[#9FA6ED] text-white flex flex-row items-center font-black`}
+      >
+        <img
+          src={"/radio_3d.png"}
+          alt="radio icon"
+          class={tw`h-8`}
+        />
+        <p class={tw`ml-2 mt-1`}>YosLab M2 Radio 2022</p>
+      </header>
+      <div class={tw`p-6 mx-auto max-w-screen-md`}>
+        hello
+      </div>
     </div>
   );
 }
