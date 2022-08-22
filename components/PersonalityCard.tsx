d82c1c686c5676a7fe21c7a1b8d5bf84a4797e82
katasu-git <katasu9923@gmail.com>
2022-08-22 23:31:21 +0900

    add p image

--- components/PersonalityCard.tsx
+++ components/PersonalityCard.tsx
@@ -13,8 +13,8 @@ export default function PersonalityCard(props: Props) {
       {props.personalities.map((personality) => (
         <div class={tw`flex flex-row mt-4`}>
           <img
-            src={`/${personality.imageName}.jpeg`}
-            alt={"t-rex"}
+            src={`/${personality.imageName}.jpg`}
+            alt={`/${personality.imageName}`}
             class={tw`block w-16 h-16 rounded-full border-2 border-[#9FA6ED]`}
             loading="lazy"
           />
