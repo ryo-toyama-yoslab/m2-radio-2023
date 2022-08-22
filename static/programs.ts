f681cdda6457028fd077c6317166b329ca8b7a60
katasu-git <katasu9923@gmail.com>
2022-08-23 00:01:32 +0900

    add contents

--- static/programs.ts
+++ static/programs.ts
@@ -2,15 +2,30 @@ import { Program } from "../types/Program.ts";
 
 const programs: Program[] = [
   {
+    "iconName": "radio",
+    "title": "オープニング",
+    "personalityLastNames": ["酒井", "鈴木", "田中", "中村", "西村", "林"],
+    overview: "まずはここから！",
+    "playtime": 10,
+  },
+  {
     "iconName": "t-rex",
     "title": "Mr.林のマシンガントーク ~ジョジョ編~",
     "personalityLastNames": ["林", "酒井"],
     overview:
-      "マシンガントークでおなじみのMr.林が，今一番熱いコンテンツについて語る！第一弾となる「ジョジョ編」では，あなたをジョジョの世界に引き込みます．",
+      "マシンガントークでおなじみのMr.林が，今一番熱いコンテンツについて語る！第1弾となる「ジョジョ編」では，あなたをジョジョの世界に引き込みます．",
     "playtime": 60,
   },
+
   {
     "iconName": "radio",
+    "title": "Mr.林のマシンガントーク ~マーベル編~",
+    "personalityLastNames": ["鈴木", "林", "田中"],
+    overview: "マシンガントークでおなじみのMr.林が，今一番熱いコンテンツについて語る！ジョジョ編ですでにお腹いっぱいの方も必聴！",
+    "playtime": 21,
+  },
+  {
+    "iconName": "index_pointing_at_the_viewer",
     "title": "このマイブームはあなたのだ！",
     "personalityLastNames": ["鈴木", "林", "田中"],
     overview: "あなたのユニークなマイブームを教えて下さい．後輩を誰よりも理解しているM2ならあなたが誰か当てられる(はず)．",
@@ -18,17 +33,10 @@ const programs: Program[] = [
   },
   {
     "iconName": "radio",
-    "title": "このマイブームはあなたのだ！",
-    "personalityLastNames": ["鈴木", "林", "田中"],
-    overview: "あなたのユニークなマイブームを教えて下さい．後輩を誰よりも理解しているM2ならあなたが誰か当てられる(はず)．",
-    "playtime": 21,
-  },
-  {
-    "iconName": "radio",
-    "title": "このマイブームはあなたのだ！",
-    "personalityLastNames": ["鈴木", "林", "田中"],
-    overview: "あなたのユニークなマイブームを教えて下さい．後輩を誰よりも理解しているM2ならあなたが誰か当てられる(はず)．",
-    "playtime": 21,
+    "title": "エンディング",
+    "personalityLastNames": ["酒井", "鈴木", "田中", "中村", "西村", "林"],
+    overview: "終わりよければ全て良し！",
+    "playtime": 10,
   },
 ];
 
