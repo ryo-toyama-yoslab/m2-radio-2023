bcefba932d94ffc5ec671b47c040120d6250f4d6
katasu-git <katasu9923@gmail.com>
2022-08-28 22:18:24 +0900

    add switcher header

--- routes/switcher.tsx
+++ routes/switcher.tsx
@@ -1,10 +1,10 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Header from "../components/Header.tsx";
 import Layout from "../components/Layout.tsx";
 import Switcher from "../islands/Switcher.tsx";
 import H1 from "../components/H1.tsx";
+import NowPlaying from "../islands/NowPlaying.tsx";
 
 export default function Home() {
   return (
@@ -16,12 +16,25 @@ export default function Home() {
         "touch-action": "manipulation",
       }}
     >
-      <Header />
-      <div id="top" />
-      <Layout>
+      <div class={tw`fixed top-0 w-full px-6 pt-6 pb-4 bg-white`}>
+        <a
+          class={tw`text-[#9FA6ED] font-bold text-sm active:underline`}
+          href="/"
+        >
+          &lt; TOP
+        </a>
+        <div class={tw`mt-2`} />
+        <NowPlaying isCompact={true} />
+        <div class={tw`mt-2`} />
         <H1 text="Switcher"></H1>
+      </div>
+      <div id="top" />
+      <div
+        class={tw`p-6 mx-auto max-w-screen-md flex flex-col justify-between`}
+      >
+        <div class={tw`mt-[161px]`} />
         <Switcher />
-      </Layout>
+      </div>
     </div>
   );
 }
