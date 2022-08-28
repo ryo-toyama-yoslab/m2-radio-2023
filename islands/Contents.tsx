f1e98827374f5ef36ccbca59f5dfa1404bd1b61a
katasu-git <katasu9923@gmail.com>
2022-08-28 21:51:44 +0900

    add contents style

--- islands/Contents.tsx
+++ islands/Contents.tsx
@@ -3,9 +3,18 @@ import { h } from "preact";
 import { tw } from "@twind";
 
 const contents = [
-  "Personalities",
-  "Programs",
-  "Gallery",
+  {
+    name: "Personalities",
+    iconPath: "man_raising_hand_3d_default",
+  },
+  {
+    name: "Programs",
+    iconPath: "studio_microphone_3d",
+  },
+  {
+    name: "Gallery",
+    iconPath: "camera_with_flash_3d",
+  },
 ];
 
 type Props = {
@@ -14,15 +23,26 @@ type Props = {
 
 export default function Contents(props: Props) {
   return (
-    <ul class={tw`text-[#9FA6ED] underline text-xl mt-4`}>
+    <ul
+      class={tw`text-[#9FA6ED] text-xl font-bold mt-4`}
+    >
       {contents.map((content, index) => {
         return (
           <li
-            class={tw`mt-1`}
-            onClick={() => props.handleOnClick(content)}
+            class={tw`mt-2 flex flex-row justify-start items-center
+            border-2 border-[#9FA6ED] rounded-[10px] px-4 py-2
+            transition-all active:opacity-50 hover:opacity-50`}
+            onClick={() => props.handleOnClick(content.name)}
             key={index}
           >
-            &gt; {content}
+            <img
+              src={`/${content.iconPath}.png`}
+              alt={content.iconPath}
+              class={tw`h-8`}
+              loading="lazy"
+              onTouchStart={(e) => ""}
+            />
+            <div class={tw`ml-4`}>{content.name}</div>
           </li>
         );
       })}
