93b78612d7d3449440a05ee139532818a2ca4cc4
katasu-git <katasu9923@gmail.com>
2022-08-30 23:56:05 +0900

    add border

--- components/ContentMagin.tsx
+++ components/ContentMagin.tsx
@@ -3,7 +3,7 @@ import { h } from "preact";
 import { tw } from "@twind";
 
 const ContentMargin = () => {
-  return <div class={tw`mt-20`} />;
+  return <div class={tw`my-10 border-b-1 rounded-full`} />;
 };
 
 export default ContentMargin;
