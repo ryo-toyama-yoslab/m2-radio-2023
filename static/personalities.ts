e60a39cbf5c4c90a4d86a91ce4c64c20ddf4dff9
katasu-git <katasu9923@gmail.com>
2022-10-13 15:53:34 +0900

    update personalities

--- static/personalities.ts
+++ static/personalities.ts
@@ -7,7 +7,6 @@ const personalities: Personality[] = [
     overview: "スーパーナチュラル",
     join: [
       "漢の恋バナ",
-      "大学（院）でやっておくべきこと",
       "Mr.林のマシンガントーク ~ジョジョ編~",
       "Mr.林のマシンガントーク ~マーベル編~",
     ],
@@ -18,7 +17,6 @@ const personalities: Personality[] = [
     LastName: "田中",
     overview: "なにわのアイコ",
     join: [
-      "田中さんPresents 吉野研雑務振り返りトーク",
       "シス工ファッション委員会",
       "みんなの10年後を語ろう",
       "大学（院）でやっておくべきこと",
@@ -33,7 +31,6 @@ const personalities: Personality[] = [
       "鈴木・西村のほろよい雑談",
       "みんなの10年後を語ろう",
       "こいつ変やなって思ったエピソード",
-      "大学（院）でやっておくべきこと",
     ],
     imageName: "suzuki",
   },
@@ -43,8 +40,6 @@ const personalities: Personality[] = [
     overview: "奈良の秘宝",
     join: [
       "シス工ファッション委員会",
-      "田中さんPresents 吉野研雑務振り返りトーク",
-      "吉野研で一番大変だったこと",
       "吉野研〇〇ランキング",
     ],
     imageName: "nakamura",
@@ -55,7 +50,6 @@ const personalities: Personality[] = [
     overview: "吉野研の妖精",
     join: [
       "鈴木・西村のほろよい雑談",
-      "このマイブームは誰のもの？",
       "こいつ変やなって思ったエピソード",
       "この際だから言っておきたいこと",
     ],
@@ -67,9 +61,8 @@ const personalities: Personality[] = [
     overview: "舌鋒鋭い",
     join: [
       "漢の恋バナ",
-      "こいつ変やなって思ったエピソード",
-      "Mr.林のマシンガントーク ~ジョジョ編~",
       "Mr.林のマシンガントーク ~マーベル編~",
+      "Mr.林のマシンガントーク ~ジョジョ編~",
     ],
     imageName: "hayashi",
   },
